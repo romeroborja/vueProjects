f9248ead379c436c2c3ba70f98d6b8cae6f24e04
Borja Romero <romeroborja96@gmail.com>
2022-06-13 23:15:21 +0200

    init

---
+++ src/components/Counter.vue
@@ -0,0 +1,61 @@
+<template>
+    <h2>  {{ title }} </h2>
+    <button v-on:click="incrementar()">+1</button>
+    <button @click="decrementar()">-1</button>
+    <p> {{ counter }} * {{ counter }} = {{ getMultiply() }} </p>
+    <p> {{ counter }} * {{ counter }} = {{ getMultiplyCache }} </p>
+</template>
+
+<script>
+export default {
+    //props: ['title'],
+    props: {
+        'start': {
+            type: Number,
+            required: true,
+            default: 10,
+            validator(val) {
+                return val > 100
+            }
+        },
+        'title': String
+    },
+    data(){
+        return {
+            counter: this.start
+        }
+    },
+    methods: {
+        getMultiply() {
+            return this.counter * this.counter;
+        },
+        incrementar() {
+            this.counter++;
+        },  
+        decrementar() {
+            this.counter--;
+        }
+    },
+    computed: {
+        // Vue guarda estos metodos en cache
+        getMultiplyCache() {
+            return this.counter * this.counter;
+        }
+    }
+}
+</script>
+
+<style>
+
+button {
+    margin: 0 5px;
+    background-color: green;
+    color: white;
+    border-radius: 5px ;
+}
+
+button:hover {
+    background-color: greenyellow;
+}
+
+</style>
